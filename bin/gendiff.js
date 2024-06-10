d269182324f36594192bd751e3882366ff411a0d
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-06-10 18:05:04 +0200

    add

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,39 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import path from 'path';
+import fs from 'fs';
+
 const program = new Command();
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format');
+  .argument('<filepath1>', 'Первый файл для сравнения')
+  .argument('<filepath2>', 'Второй файл для сравнения')
+  .option('-f, --format <type>', 'output format', 'json')
+  .action((filepath1, filepath2, type) => {
+    // Логика обработки файлов и вывода результата
+    console.log(
+      `Обрабатываю файлы: ${filepath1} и ${filepath2} по формату ${type}`
+    );
 
-program.parse();
+    const dirPath = './__fixtures__';
+
+    // Построение абсолютных путей к файлам
+    const absolutePath1 = path.resolve(dirPath, filepath1);
+    const absolutePath2 = path.resolve(dirPath, filepath2);
+
+    try {
+      const result1 = fs.readFileSync(absolutePath1, { encoding: 'utf-8' });
+      const result2 = fs.readFileSync(absolutePath2, { encoding: 'utf-8' });
+
+      console.log('Содержимое файла 1:', result1);
+      console.log('Содержимое файла 2:', result2);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+program.parse(process.argv);
+//.addValidator() метод валидации файлов
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-const path = require('path');
-const fs = require('fs');
-
-// import fs from 'fs/promises';
 // import path from 'path';
+// import fs from 'fs';
+// import { filepath1, filepath2 } from '/root/frontend-project-46/bin';
+
+// const dirPath = './__fixtures__';
+// const fileName = filepath1;
+// const filePath = path.join(dirPath, fileName);
+
+// try {
+//   const result = fs.readFileSync(filePath, { encoding: 'utf-8' });
+
+//   console.log(result);
+// } catch (err) {
+//   console.error(err);
+// }
