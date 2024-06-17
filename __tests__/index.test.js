d6972db09a9e2e3da7f3e0bbaf90e92d3f06e6c5
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-06-17 15:44:00 +0200

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,20 +1,9 @@
-import { fileURLToPath } from 'url';
-import fs from 'fs';
-import path from 'path';
+import { readFile } from '../src/utils.js';
 import genDiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const getFixturePath = (filename) =>
-  path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(filename, 'utf-8');
-
-let expected = readFile(getFixturePath('expected_file.json.txt'));
+let expected = readFile('expected_file.json.txt');
 
 test('genDiff compares two JSON files correctly', () => {
-  const firstPath = getFixturePath('file1.json');
-  const secondPath = getFixturePath('file2.json');
-  const result = genDiff(firstPath, secondPath, 'json');
+  const result = genDiff('file1.json', 'file2.json', 'json');
   expect(result).toEqual(expected);
 });
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,13 @@
+import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
 
-export const readFile = (filePath) => {
-  const dirName = process.cwd();
-  const fullPath = path.resolve(dirName, filePath);
-  return fs.readFileSync(fullPath, 'utf-8');
-};
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const getFixturePath = (filename) =>
+  path.join(__dirname, '..', '__fixtures__', filename);
+
+export const readFile = (filename) =>
+  fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 export const parse = (file) => {
   return JSON.parse(file);
