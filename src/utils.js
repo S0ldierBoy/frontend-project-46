0bf18e2ae940ea5d4b0c31ba27f99f8555f111ce
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-06-28 03:37:55 +0200

    fix

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,5 @@
-import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const getFullPath = (filePath) => path.resolve(filePath);
 
