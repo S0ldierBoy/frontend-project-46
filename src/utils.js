44799d284277474a8cf349bf9e7e078189d56854
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-06-28 02:58:06 +0200

    fix

--- src/utils.js
+++ src/utils.js
@@ -5,10 +5,12 @@ import fs from 'fs';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export const getFixturePath = (filename) => {
-  return path.join(__dirname, '..', '__fixtures__', filename);
+  const paths = path.join(__dirname, '..', '__fixtures__', filename);
+  return paths
 };
 
 export const readFile = (filename) => {
   const filePath = getFixturePath(filename);
-  return fs.readFileSync(filePath, 'utf-8');
+  const file = fs.readFileSync(filePath, 'utf-8');
+  return file
 };
