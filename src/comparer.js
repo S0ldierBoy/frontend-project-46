0861ef11325f1448a7cc75c53d70f7c5df6648bb
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-06-29 17:29:08 +0200

    fix

--- src/comparer.js
+++ src/comparer.js
@@ -1,11 +1,11 @@
 import _ from 'lodash';
 
 const comparer = (obj1, obj2) => {
-  const keys = _.union(_.keys(obj1), _.keys(obj2)); // Получаем объединение ключей без сортировки
+  const keys = _.union(_.keys(obj1), _.keys(obj2)); // Получаем объединение ключей
 
-  keys.sort(); // Сортируем массив ключей
+  const sortedKeys = _.sortBy(keys); // Создаем новый массив с отсортированными ключами
 
-  return keys.map((key) => {
+  return sortedKeys.map((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
     if (
@@ -24,7 +24,10 @@ const comparer = (obj1, obj2) => {
     }
     if (!_.isEqual(value1, value2)) {
       return {
-        key, type: 'changed', oldValue: value1, newValue: value2,
+        key,
+        type: 'changed',
+        oldValue: value1,
+        newValue: value2,
       };
     }
     return { key, type: 'unchanged', value: value1 };
