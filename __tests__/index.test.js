d47c9529afbd2cf647dc4c640179e20fa59287de
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-06-29 15:17:30 +0200

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,7 +6,8 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) =>
+  path.join(__dirname, '..', '__fixtures__', filename);
 
 const readFixture = (filename) => {
   const filePath = getFixturePath(filename);
