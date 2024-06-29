eae66f8a4597a657465d0648f24aabb042c504c9
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-06-29 17:21:15 +0200

    add and fix

--- src/comparer.js
+++ src/comparer.js
@@ -3,7 +3,9 @@ import _ from 'lodash';
 const comparer = (obj1, obj2) => {
   const keys = _.union(_.keys(obj1), _.keys(obj2)).sort();
 
-  return keys.map((key) => {
+  const sortedKeys = [...keys].sort(); // Создаем копию массива и сортируем его
+
+  return sortedKeys.map((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
     if (
@@ -22,7 +24,10 @@ const comparer = (obj1, obj2) => {
     }
     if (!_.isEqual(value1, value2)) {
       return {
-        key, type: 'changed', oldValue: value1, newValue: value2,
+        key,
+        type: 'changed',
+        oldValue: value1,
+        newValue: value2,
       };
     }
     return { key, type: 'unchanged', value: value1 };
