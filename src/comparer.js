522c76543ccea2d52f6d885cdef91a192c3c8515
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-07-01 14:46:34 +0200

    update

--- src/comparer.js
+++ src/comparer.js
@@ -8,18 +8,13 @@ const comparer = (obj1, obj2) => {
   return sortedKeys.map((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
-    if (
-      _.isObject(value1)
-      && _.isObject(value2)
-      && !Array.isArray(value1)
-      && !Array.isArray(value2)
-    ) {
+    if (_.isObject(value1) && _.isObject(value2)) {
       return { key, type: 'nested', children: comparer(value1, value2) };
     }
-    if (!_.hasIn(obj1, key)) {
+    if (!_.has(obj1, key)) {
       return { key, type: 'added', value: value2 };
     }
-    if (!_.hasIn(obj2, key)) {
+    if (!_.has(obj2, key)) {
       return { key, type: 'removed', value: value1 };
     }
     if (!_.isEqual(value1, value2)) {
