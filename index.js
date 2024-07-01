fafbf25a4390b115339c1d4433a230c8bb2752b3
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-07-01 15:44:13 +0200

    fix and update

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-import { extname } from 'path';
-import readFile from './src/utils.js';
-import comparer from './src/comparer.js';
-import parse from './src/parsers.js';
-import outputController from './src/formatters/index.js';
-
-const genDiff = (file1, file2, format) => {
-  const fileContent1 = readFile(file1);
-  const fileContent2 = readFile(file2);
-
-  const ext1 = extname(file1).slice(1);
-  const ext2 = extname(file2).slice(1);
-
-  const parsedData1 = parse(fileContent1, ext1);
-  const parsedData2 = parse(fileContent2, ext2);
-
-  const comparedFiles = comparer(parsedData1, parsedData2);
-
-  const resultDiff = outputController(comparedFiles, format);
-  return resultDiff;
-};
+import genDiff from './src/genDiff.js';
 
 export default genDiff;
---
+++ src/genDiff.js
@@ -0,0 +1,23 @@
+import { extname } from 'path';
+import readFile from './utils.js';
+import comparer from './comparer.js';
+import parse from './parsers.js';
+import outputController from './formatters/index.js';
+
+const genDiff = (file1, file2, format) => {
+  const fileContent1 = readFile(file1);
+  const fileContent2 = readFile(file2);
+
+  const ext1 = extname(file1).slice(1);
+  const ext2 = extname(file2).slice(1);
+
+  const parsedData1 = parse(fileContent1, ext1);
+  const parsedData2 = parse(fileContent2, ext2);
+
+  const comparedFiles = comparer(parsedData1, parsedData2);
+
+  const resultDiff = outputController(comparedFiles, format);
+  return resultDiff;
+};
+
+export default genDiff;
