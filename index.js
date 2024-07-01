c43f1a7446ac8d5dcf88ad97c23f61a12decbd59
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-07-01 15:27:10 +0200

    fix

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const genDiff = (file1, file2, format) => {
   const fileContent1 = readFile(file1);
   const fileContent2 = readFile(file2);
 
-  const ext1 = extname(file1);
-  const ext2 = extname(file2);
+  const ext1 = extname(file1).slice(1);
+  const ext2 = extname(file2).slice(1);
 
   const parsedData1 = parse(fileContent1, ext1);
   const parsedData2 = parse(fileContent2, ext2);
--- src/parsers.js
+++ src/parsers.js
@@ -2,10 +2,10 @@ import yaml from 'js-yaml';
 
 const parse = (data, ext) => {
   switch (ext) {
-    case '.json':
+    case 'json':
       return JSON.parse(data);
-    case '.yml':
-    case '.yaml':
+    case 'yml':
+    case 'yaml':
       return yaml.load(data);
     default:
       throw new Error(`Unsupported file format: ${ext}`);
